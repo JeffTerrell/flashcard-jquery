99a803aa7303c4a5a9b152eeacbb54a22ade5204
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-19 18:03:34 -0400

    make definitions disappear and re-appear with js function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".section h2").click(function() {
+    $("p").slideToggle();
+  });
+});
+
+// $(document).ready(function() {
+//   $(".click").click(function() {
+//     $("#show").toggle();
+//     $("#hide").toggle();
+//   });
+// });
