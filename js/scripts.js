e63ee0b7e6885d5866325701229eeeeffab32660
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-19 18:09:06 -0400

    add bootstrap container
    Co-authored-by: Kate Kiatsiri<keidsiri@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $(".section h2").click(function() {
     $("p").slideToggle();
   });
+
+  $(".section p").click(function() {
+    $("p").slideToggle();
+  });
 });
 
 // $(document).ready(function() {
